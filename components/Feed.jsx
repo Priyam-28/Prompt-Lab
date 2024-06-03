8e43905bb98e67bfc885eb56d67b73ea49d6a16d
UnknownRider <barikpriyam28@gmail.com>
2024-06-03 13:07:07 +0530

    Feed API route

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+'use client'
 
-const Feed = () => {
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PrompCardList = ({ data, handleTagClick }) => {
   return (
-    <div>
-      Feed
+
+    <div className="mt-16 prompt_layout">
+      {data.map((post)=>{
+        <PromptCard
+        key={post.id}
+        post={post}
+        handleTagClick={handleTagClick}/>
+      })}
+      Prompt Card
+
     </div>
+
   )
 }
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState();
+  const [posts,setPosts]=useState([]);
+  const handleSearchChange = (e) => {
+
+  }
+  useEffect(() => {
+    const fetchPosts = async() => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json();
+      console.log(data)
+      setPosts(data)
+
+
+    }
+    fetchPosts();
+
+  }, [])
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input type="text"
+          placeholder="Search For a Tag or Username"
+          value={searchText}
+          onChange={
+            handleSearchChange
+          }
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PrompCardList
+        data={[]}
+        handleTagClick={() => {
+
+        }} />
+
+    </section>
+  )
+}
+
+
 
 export default Feed
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,25 +1,11 @@
-import PromptCard from "./PromptCard";
+import React from 'react'
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+const PromptCard = ({posts,handleTagClick,HandleEdit,handleDelete}) => {
   return (
-    <section className='w-full'>
-      <h1 className='head_text text-left'>
-        <span className='blue_gradient'>{name} Profile</span>
-      </h1>
-      <p className='desc text-left'>{desc}</p>
+    <div>
+      
+    </div>
+  )
+}
 
-      <div className='mt-10 prompt_layout'>
-        {data.map((post) => (
-          <PromptCard
-            key={post._id}
-            post={post}
-            handleEdit={() => handleEdit && handleEdit(post)}
-            handleDelete={() => handleDelete && handleDelete(post)}
-          />
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default Profile;
+export default PromptCard
